89ce566ab6a5aacec30b4c7c81a880c6d8ab0f7f
Charlie <howesybusiness@gmail.com>
2020-09-17 14:28:50 +0100

    Added example code.

---
+++ Example.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DiscordTokenRetriever;
+
+namespace DiscordTokenRetrieverLibrary
+{
+    class Example
+    {
+        void exampleFunction()
+        {
+            foreach (string token in TokenRetriever.RetrieveDiscordTokens())
+            {
+                Console.WriteLine($"Found a token! {token}");
+            }
+        }
+
+    }
+}
